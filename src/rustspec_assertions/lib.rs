c516c13576e4e95da1d3c230d0618c26a57d7ac2
Unai Orbe <unai@orbe.me>
2014-08-10 12:53:05 +0100

    get rid of warnings

--- src/rustspec_assertions/lib.rs
+++ src/rustspec_assertions/lib.rs
@@ -1,14 +1,19 @@
 #![crate_name="rustspec_assertions"]
 #![crate_type="lib"]
 
+pub use matchers::equals::Equals;
 pub use matchers::equals::eq;
 pub use equal = matchers::equals::eq;
+pub use matchers::gt::Gt;
 pub use matchers::gt::be_gt;
 pub use be_greater_than = matchers::gt::be_gt;
+pub use matchers::ge::Ge;
 pub use matchers::ge::be_ge;
+pub use matchers::lt::Lt;
 pub use be_greater_or_equal_to = matchers::ge::be_ge;
 pub use matchers::lt::be_lt;
 pub use be_less_than = matchers::lt::be_lt;
+pub use matchers::le::Le;
 pub use matchers::le::be_le;
 pub use be_less_or_equal_to = matchers::le::be_le;
 pub use expect::expect;
