3b6413bf0696e1640ebcba24d69643f11f1390aa
Unai Orbe <unai@orbe.me>
2014-08-10 11:30:55 +0100

    oops messages were always negated

--- src/rustspec_assertions/expect.rs
+++ src/rustspec_assertions/expect.rs
@@ -4,29 +4,41 @@ use self::core::fmt::Show;
 use matchers::matcher::Matcher;
 
 pub struct Expect<T> {
-    value: T
+    value: T,
+    negated: bool
 }
 
 impl<T: Show + Clone> Expect<T> {
     pub fn to(&self, other: Box<Matcher<T>>) {
-        if !other.assert_check(self.value.clone()) {
+        let mut cond = !other.assert_check(self.value.clone());
+
+        if self.negated {
+            cond = !cond;
+        }
+
+        if cond {
             self.fail(other)
         }
     }
 
-    pub fn not_to(&self, other: Box<Matcher<T>>) {
-        if other.assert_check(self.value.clone()) {
-            self.fail(other)
-        }
+    pub fn not_to(&mut self, other: Box<Matcher<T>>) {
+        self.negated = true;
+        self.to(other);
     }
 
     fn fail(&self, other: Box<Matcher<T>>) {
-        // let mut t = term::stdout().unwrap();
-        // t.fg(term::color::RED).unwrap();
-        fail!(other.negated_msg(self.value.clone()));
+        let mut msg;
+
+        if self.negated {
+            msg = other.negated_msg(self.value.clone());
+        } else {
+            msg = other.msg(self.value.clone());
+        }
+
+        fail!(msg);
     }
 }
 
 pub fn expect<T: Eq + Ord + Show>(value: T) -> Expect<T> {
-    Expect { value: value }
+    Expect { value: value, negated: false }
 }
