0e8ce5277aa3ec6bdd38f923ac8c9b2c91b713d2
Unai Orbe <unai@orbe.me>
2014-08-17 11:17:26 +0100

    go back to using line! until parser issue is resolved

--- src/rustspec_assertions/lib.rs
+++ src/rustspec_assertions/lib.rs
@@ -17,29 +17,37 @@ pub use matchers::le::Le;
 pub use matchers::le::be_le;
 pub use expect::expect;
 
-use syntax::ext::base;
-use syntax::ext::base::{ExtCtxt, MacResult};
-use syntax::codemap::{Span};
-use syntax::ast::TokenTree;
-use rustc::plugin::Registry;
-use syntax::ext::build::AstBuilder;
+// use syntax::ext::base;
+// use syntax::ext::base::{ExtCtxt, MacResult};
+// use syntax::codemap::{Span};
+// use syntax::ast::TokenTree;
+// use rustc::plugin::Registry;
+// use syntax::ext::build::AstBuilder;
 
-#[plugin_registrar]
-pub fn plugin_registrar(registry: &mut Registry) {
-    registry.register_macro("expand_line", expand_line);
-}
+// #[plugin_registrar]
+// pub fn plugin_registrar(registry: &mut Registry) {
+//     registry.register_macro("expand_line", expand_line);
+// }
 
-// report the appropiate line when assertion is summoned inside a macro.
-pub fn expand_line(cx: &mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<MacResult> {
-    base::check_zero_tts(cx, sp, tts, "expand_line!");
+// // report the appropiate line when assertion is summoned inside a macro.
+// pub fn expand_line(cx: &mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<MacResult> {
+//     base::check_zero_tts(cx, sp, tts, "expand_line!");
 
-    let topmost = cx.backtrace().unwrap()
-        .call_site.expn_info.unwrap();
+//     let topmost = cx.backtrace().unwrap()
+//         .call_site.expn_info.unwrap();
 
-    let loc = cx.codemap().lookup_char_pos(topmost.call_site.lo);
+//     let loc = cx.codemap().lookup_char_pos(topmost.call_site.lo);
 
-    base::MacExpr::new(cx.expr_uint(topmost.call_site, loc.line))
-}
+//     base::MacExpr::new(cx.expr_uint(topmost.call_site, loc.line))
+// }
+
+#[macro_export]
+pub macro_rules! expand_line(
+    () => (
+        line!()
+    );
+)
+
 
 pub mod expect;
 mod matchers {
